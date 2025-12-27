01c725a455bf4c5a1c04c3c76400f48f92d8c2aa
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-12-27 15:27:52 +0530

    Enable configuration properties scanning and scheduling in EventRelayApplication

--- src/main/java/com/ansh/EventRelay/EventRelayApplication.java
+++ src/main/java/com/ansh/EventRelay/EventRelayApplication.java
@@ -2,8 +2,12 @@ package com.ansh.EventRelay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 public class EventRelayApplication {
 
 	public static void main(String[] args) {
