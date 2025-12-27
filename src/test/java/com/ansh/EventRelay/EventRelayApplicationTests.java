f9f80b10f81ee5916b6bb759749bedb105c8a809
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-12-27 15:35:15 +0530

    Enhance EventRelayApplicationTests with MockBean for WebhookEventRepository and update Spring Boot test properties to exclude specific auto-configurations

--- src/test/java/com/ansh/EventRelay/EventRelayApplicationTests.java
+++ src/test/java/com/ansh/EventRelay/EventRelayApplicationTests.java
@@ -1,10 +1,16 @@
 package com.ansh.EventRelay;
 
+import com.ansh.EventRelay.events.WebhookEventRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class EventRelayApplicationTests {
+	@MockBean
+	WebhookEventRepository webhookEventRepository;
 
 	@Test
 	void contextLoads() {
